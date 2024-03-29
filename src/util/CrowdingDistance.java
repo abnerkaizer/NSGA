bd3d90991c32f2e921a3814f37e0448ad289c633
Abner Kaizer <abnerkaizer@protonmail.com>
2024-03-28 21:46:32 -0300

    Refactoring.

--- src/util/CrowdingDistance.java
+++ src/util/CrowdingDistance.java
@@ -6,26 +6,24 @@ import java.util.List;
 
 public class CrowdingDistance {
     private final QuickSort qs = new QuickSort();
-    public void avaliar(List<Individuo> T) {
-        int s = T.size();
-        for (Individuo ind : T) {
+
+    public void avaliar(List<Individuo> individuos) {
+        int s = individuos.size();
+        for (Individuo ind : individuos) {
             ind.setD(0);
         }
-        Individuo ind0 = T.getFirst();
+        Individuo ind0 = individuos.getFirst();
         int M = ind0.getObjs().length;
         for (int m = 0; m < M; m++) {
-            qs.sort(T, m);
-            T.getFirst().setD(Double.POSITIVE_INFINITY);
-            T.getLast().setD(Double.POSITIVE_INFINITY);
+            qs.sort(individuos, m);
+            individuos.getFirst().setD(Double.POSITIVE_INFINITY);
+            individuos.getLast().setD(Double.POSITIVE_INFINITY);
             for (int i = 1; i < s - 1; i++) {
-                Individuo antInd = T.get(i - 1);
-                Individuo posInd = T.get(i + 1);
-                double aux = T.get(i).getD()+((posInd.getObjs()[m] - antInd.getObjs()[m]) / (T.getLast().getObjs()[m] - T.getFirst().getObjs()[m]));
-                T.get(i).setD(aux);
+                Individuo antInd = individuos.get(i - 1);
+                Individuo posInd = individuos.get(i + 1);
+                double aux = individuos.get(i).getD() + ((posInd.getObjs()[m] - antInd.getObjs()[m]) / (individuos.getLast().getObjs()[m] - individuos.getFirst().getObjs()[m]));
+                individuos.get(i).setD(aux);
             }
         }
-        System.out.println();
     }
-
-
 }
